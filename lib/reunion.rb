cd509250b21ea42df9373d43327d415d92a11274
Phil DeLong <philjdelong@gmail.com>
2019-10-27 22:51:38 -0600

    Create helper methods for participants and activity cost

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -10,10 +10,24 @@ class Reunion
     @activities << activity
   end
 
+  def activity_cost(activity)
+    activity.total_cost
+  end
+
   def total_cost
     @activities.flat_map do |activity|
-      activity.participants.values
+      activity_cost(activity)
     end.sum
+  end
+
+  def activity_participants(activity)
+    activity.participants
+  end
+
+  def all_participants
+    @activities.flat_map do |activity|
+      activity_participants(activity)
+    end
   end
 
   def breakout
