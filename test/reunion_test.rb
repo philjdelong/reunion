85af2587ae3bbfc302f4954c2e23a22c4c6cffbe
Phil DeLong <philjdelong@gmail.com>
2019-11-01 11:57:49 -0600

    Iteration 2 reset

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -24,43 +24,4 @@ class ReunionTest < Minitest::Test
     @reunion.add_activity(@activity_1)
     assert_equal [@activity_1], @reunion.activities
   end
-
-  def test_it_can_calculate_activity_cost
-    @activity_1.add_participant("Maria", 20)
-    @activity_1.add_participant("Luther", 40)
-    @reunion.add_activity(@activity_1)
-    assert_equal 60, @reunion.activity_cost(@activity_1)
-  end
-
-  def test_it_can_calculate_total_cost
-    @activity_1.add_participant("Maria", 20)
-    @activity_1.add_participant("Luther", 40)
-    @reunion.add_activity(@activity_1)
-    assert_equal 60, @reunion.total_cost
-    # skip
-    @activity_2.add_participant("Maria", 60)
-    @activity_2.add_participant("Luther", 60)
-    @activity_2.add_participant("Louis", 0)
-    @reunion.add_activity(@activity_2)
-    assert_equal 180, @reunion.total_cost
-  end
-
-  def test_it_can_breakout_cost_amongst_participants
-    skip
-    @activity_1.add_participant("Maria", 20)
-    @activity_1.add_participant("Luther", 40)
-    @reunion.add_activity(@activity_1)
-    @activity_2.add_participant("Maria", 60)
-    @activity_2.add_participant("Luther", 60)
-    @activity_2.add_participant("Louis", 0)
-    @reunion.add_activity(@activity_2)
-    assert_equal ({"Maria" => -10, "Luther" => -30, "Louis" => 40}), @reunion.breakout
-  end
-
-  def test_it_can_provide_reunion_summary
-    skip
-    assert_equal "Maria: -10\nLuther: -30\nLouis: 40", @reunion.summary
-  end
-
-# puts reunion.summary
 end
